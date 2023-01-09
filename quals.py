7321ecb507f3b410863fb29ca3751bbe4342579a
uku <uku3lig@gmail.com>
2023-01-09 19:21:46 +0100

    add player data to bracket

--- quals.py
+++ quals.py
@@ -5,6 +5,7 @@ import json
 import os.path
 import sys
 
+import requests
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -36,6 +37,7 @@ def get_sheet_data(spreadsheet_id: str, range_name: str, scopes: [str]) -> [[str
             token.write(creds.to_json())
 
     try:
+        print(f"getting google sheet info on range `{range_name}`")
         service = build('sheets', 'v4', credentials=creds)
 
         # Call the Sheets API
@@ -45,6 +47,13 @@ def get_sheet_data(spreadsheet_id: str, range_name: str, scopes: [str]) -> [[str
     except HttpError as err:
         print(err)
         sys.exit(1)
+
+
+def get_player_data(player: int) -> dict:
+    with open("osu-token.txt", "r") as f:
+        token = f.readline()
+        response = requests.get(f"https://osu.ppy.sh/api/get_user?k={token}&u={player}&m=0").json()
+        return response[0]
 
 
 def get_z_sums(values: [[str]]) -> [float]:
@@ -59,9 +68,18 @@ def get_teams(values: [[str]], sums: [[str]], players: [[str]]) -> [object]:
 
     # first 2 rows are mods (NM1 etc.) and each row has team name + each map score (MAPS + 1)
     for row, z_sum in zip([r for r in values[2:] if len(r) == (MAPS_AMOUNT + 1)], sums_avg):
-        seeding_results = []
-        player_ids = [{"id": int(team_info[1])} for team_info in players if team_info[0] == row[0]]
+        player_ids = [int(team_info[1]) for team_info in players if team_info[0] == row[0]]
+        player_infos = []
+        for player in player_ids:
+            data = get_player_data(player)
+            print(f"getting player info for {data['username']}")
+            player_infos.append({
+                "id": player,
+                "Rank": int(data["pp_rank"]),
+                "country_code": data["country"]
+            })
 
+        seeding_results = []
         # groups maps by mod, skips first column (= team name)
         # tpl is the (int, str) tuple given by enumerate, tpl[0] = column index
         groups = itertools.groupby(enumerate(row[1:], start=1), key=lambda tpl: mods[tpl[0]])
@@ -84,7 +102,8 @@ def get_teams(values: [[str]], sums: [[str]], players: [[str]]) -> [object]:
             "Seed": -1,
             "_ZSUM": z_sum,
             "SeedingResults": seeding_results,
-            "Players": player_ids
+            "Players": player_infos,
+            "FlagName": player_infos[0]["country_code"]
         }
 
 
